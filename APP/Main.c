efca14fb1771f58423e4c217420b7ad066736b29
lz00qs <lz00qs@gmail.com>
2022-11-14 19:43:20 +0800

    Change file code to UTF8

--- APP/Main.c
+++ APP/Main.c
@@ -17,9 +17,9 @@ void Main_Circulation()
 
 void DebugInit(void)
 {
-  GPIOA_SetBits(GPIO_Pin_9);
-  GPIOA_ModeCfg(GPIO_Pin_8, GPIO_ModeIN_PU);
-  GPIOA_ModeCfg(GPIO_Pin_9, GPIO_ModeOut_PP_5mA);
+  GPIOB_SetBits(GPIO_Pin_19);
+  GPIOB_ModeCfg(GPIO_Pin_19, GPIO_ModeIN_PU);
+  GPIOB_ModeCfg(GPIO_Pin_19, GPIO_ModeOut_PP_5mA);
   UART1_DefInit();
 }
 
